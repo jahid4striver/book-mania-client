96154bdd4bf14b46428d1f4e45b957128b95014b
jahid4striver <jahidhassan701@gmail.com>
2023-08-29 06:25:41 +0600

    Book Delete Function Created

--- src/components/BookDetailsCard.tsx
+++ src/components/BookDetailsCard.tsx
@@ -1,8 +1,20 @@
+import { useDeleteBookMutation } from "@/redux/features/books/bookApi";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const BookDetailsCard = (data: any) => {
     const book = data?.data;
     const navigate = useNavigate();
+    const [deleteBook] = useDeleteBookMutation()
+
+    const handleDeleteBook = (id: string) => {
+        const proceed = window.confirm('Are You Sure Want To Delete');
+        if (proceed) {
+            deleteBook(id);
+            toast('Book Delete Successful');
+            navigate('/allbooks');
+        }
+    }
 
     return (
         <>
@@ -19,7 +31,7 @@ const BookDetailsCard = (data: any) => {
                     <p>Genre: {book?.genre}</p>
                     <div className="mt-2 flex gap-2">
                         <button onClick={() => navigate(`/edit/${book?._id}`)} className="btn btn-sm btn-primary text-white font-bold">Edit</button>
-                        <button className="btn btn-sm btn-error text-white font-bold">Delete</button>
+                        <button onClick={() => handleDeleteBook(book?._id)} className="btn btn-sm btn-error text-white font-bold">Delete</button>
                     </div>
                 </div>
                 {/* <div>
